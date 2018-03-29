8f4f985ea50b18e3b4b07e3d65149a142bdb5a7e
Stanislav <Gmapll9669@Gmail.com>
2018-03-29 23:30:27 +0300

    last1
    last

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
 var button = document.querySelector(".button__search");
 var form = document.querySelector(".search-form");
+var arrivalData = document.querySelector("[name=arrival-data]");
+var departureData = document.querySelector("[name=departure-data]");
+var numberAdults = document.querySelector("[name=number-adults]");
+var numberChildren = document.querySelector("[name=number-children]");
+
 
 button.addEventListener("click", function () {
 	form.classList.toggle("form-search");
+	arrivalData.focus();
+});
+
+form.addEventListener("submit", function (evt) {
+	if (!arrivalData.value || !departureData.value || !numberAdults.value || !numberChildren.value) {
+		evt.preventDefault();
+		form.classList.remove("form-error");
+		form.offsetWidth = form.offsetWidth;
+    form.classList.add("form-error");
+	}
+
 });
